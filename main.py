be1ed7a56e7810a02ede79fde425e8ee2dedeb5d
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-31 15:42:47 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class validatorUncryptWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = validatorUncryptWorker()
+    print("Worker engine initialized.")
